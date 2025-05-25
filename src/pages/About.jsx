2315b9b159824b82e1e10b30c3e64fa80b58c026
Adnan <mohammadaslamadnan@gmail.com>
2025-05-25 16:01:23 +0530

    Home, About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,31 @@
+import Section2 from "../components/About/Section2"
+import Section3 from "../components/About/Section3"
+import Section4 from "../components/About/Section4"
+import Section5 from "../components/About/Section5"
+import Section6 from "../components/About/Section6"
 
 const About = () => {
    return <>
-       <div>
-           About
-       </div>
+      <section className="relative  z-10 w-screen h-full">
+                  <div className="absolute  z-20 inset-0 bg-[url('./assets/images/home/home-hero.jpg')] bg-cover"></div>
+                  <div className="py-55 relative z-30 flex justify-center items-center bg-[#00000099]">
+                      <div className="w-1/2 flex flex-col justify-start items-center">
+                          <p className="text-white text-lg w-fit bg-[#88888888] py-1 px-9 rounded-3xl">{'About us'.toUpperCase()}</p>
+                          <h1 className="text-white w-[900px] text-7xl font-Playfair Display py-5 text-center">Established with a passion for exploration
+      </h1>               <h6 className="text-white"></h6>
+                          <div className="flex mt-15 gap-8">
+                          </div>
+                         
+                      </div>
+                  </div>
+              </section>
+
+
+              <Section2/>
+              <Section3/>
+              <Section4/>
+              <Section5/>
+              <Section6/>
    </>
 }
 
